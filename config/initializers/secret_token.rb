464e99323ab0f976ed618d7849128e7d5fc8157a
James Mac William <jmacwilliam@constantcontact.com>
2012-12-22 11:54:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheapEats::Application.config.secret_token = 'ce5cbdff7309b8e61248a380e566dbd4690c136d73662d12a7ff6cb2199a949630c2a89d45a28223410444a0af1888b35770a0dbf9bcdddd43733f348b8fb65c'
